c30e38b52e1db6d808727365d4b6010c198b1191
Erik Schoster <erik@heythere.org>
2011-11-18 03:25:22 -0600

    wes and song

--- magill.py
+++ magill.py
@@ -480,7 +480,7 @@ class Magill:
         p['voicespeed'] = 2.6
         p['voices'] = [dsp.amp(b_chorus_a, 0.9), dsp.amp(c_chorus_a, 0.4)]
         p['guitars'] = ([g1], [g1, g3])
-        big = dsp.cut(self.sing(p), 0, dsp.stf(8))
+        big = self.sing(p)
 
         snd = {'ad': self.ad.data }
         dinggroup = [
@@ -501,10 +501,10 @@ class Magill:
         numhats = dsp.flen(big) / (dsp.beat / 3)
         hat = dsp.alias(dsp.cut(self.ad.data, 0, dsp.mstf(30)))
         nohat = dsp.pad('', dsp.mstf(30), 0)
-        hatz = [dsp.pad(dsp.pulsar(random.choice([dsp.alias(hat), nohat])), 0, (dsp.beat / 3) - dsp.mstf(30)) for i in range(numhats)]
+        hatz = [dsp.pad(dsp.pulsar(random.choice([dsp.alias(hat), nohat, hat, hat])), 0, (dsp.beat / 3) - dsp.mstf(30)) for i in range(numhats)]
         hats = dsp.mix([dsp.amp(''.join(hatz), 1.2), hats])
 
-        out += dsp.mix([hats, big, dsp.amp(dings, 0.5)])
+        out += dsp.mix([hats, dsp.amp(big, 0.5), dsp.amp(dings, 0.5)])
 
         # dings
         p['voicerand'] = True
@@ -514,23 +514,23 @@ class Magill:
         out += self.sing(p)
 
         # should we all wake up
-        dsp.beat = dsp.bpm2frames(90.0)
-        p['voicerand'] = False 
-        p['voicespeed'] = 3.6
-        p['voices'] = [dsp.amp(b_chorus_c, 1.2), dsp.amp(c_chorus_c, 0.8)]
-        p['guitars'] = ([g1], [g3])
-        big = dsp.cut(self.sing(p), 0, dsp.stf(6))
+        #dsp.beat = dsp.bpm2frames(90.0)
+        #p['voicerand'] = False 
+        #p['voicespeed'] = 3.6
+        #p['voices'] = [dsp.amp(b_chorus_c, 1.2), dsp.amp(c_chorus_c, 0.8)]
+        #p['guitars'] = ([g1], [g3])
+        #big = self.sing(p)
 
-        dinggroup = [
-            self.patterns.dingstreamA(snd, dsp.flen(big), 0.5, 2.667 * 2),
-            self.patterns.dingstreamA(snd, dsp.flen(big), 0.33333, 2.667 * 2),
-            self.patterns.dingstreamA(snd, dsp.flen(big), 0.5, 2.667, 1.2),
-            self.patterns.dingstreamA(snd, dsp.flen(big), 0.75, 2.667),
-            self.patterns.dingstreamA(snd, dsp.flen(big), 1.25, 2.667, 1.1),
-            ]
-        dings = dsp.fill(dsp.mix(dinggroup), dsp.flen(big))
+        #dinggroup = [
+            #self.patterns.dingstreamA(snd, dsp.flen(big), 0.5, 2.667 * 2),
+            #self.patterns.dingstreamA(snd, dsp.flen(big), 0.33333, 2.667 * 2),
+            #self.patterns.dingstreamA(snd, dsp.flen(big), 0.5, 2.667, 1.2),
+            #self.patterns.dingstreamA(snd, dsp.flen(big), 0.75, 2.667),
+            #self.patterns.dingstreamA(snd, dsp.flen(big), 1.25, 2.667, 1.1),
+            #]
+        #dings = dsp.fill(dsp.mix(dinggroup), dsp.flen(big))
 
-        out += dsp.mix([big, dsp.amp(dings, 0.5)])
+        #out += dsp.mix([big, dsp.amp(dings, 0.5)])
 
         return out
 
