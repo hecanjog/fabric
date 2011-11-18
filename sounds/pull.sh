a45c96056b4fd478f0f7c7f139f2d0afc127084e
Erik Schoster <erik@hecanjog.com>
2011-11-17 20:49:22 -0800

    render dir placeholder

--- sounds/pull.sh
+++ sounds/pull.sh
@@ -4,15 +4,6 @@
 # Pulls sounds from magic.hecanjog.com
 ######################################
 
-
-##
-# Vorms remix for Collections of Colonies of Bees
-rsync -avz somagical@magic.hecanjog.com:fabric/sounds/vorms/ vorms/ 
-
-##
-# 1of by Rough Weather
-rsync -avz somagical@magic.hecanjog.com:fabric/sounds/1of/ 1of/
-
 ##
 # Computer Music With Voices interlude for Eric & Magill
-#rsync -avz somagical@magic.hecanjog.com:fabric/sounds/magill_interlude/ magill_interlude/
+rsync -avz somagical@magic.hecanjog.com:fabric/sounds/magill_interlude/ magill_interlude/
