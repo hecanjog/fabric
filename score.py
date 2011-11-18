a2b6ccd7d94760a1acd73c22a46915832406218d
Erik Schoster <erik@heythere.org>
2011-11-18 13:25:16 -0600

    more synth tests

--- score.py
+++ score.py
@@ -14,9 +14,9 @@ def main(out=''):
     score = Score()
     dsp.beat = dsp.bpm2frames(88.0)
 
-    rough = dsp.mix([score.opening(dsp.stf(360), 'random') for i in range(3)])
-    roughy = dsp.mix([score.opening(dsp.stf(360), 'line') for i in range(3)])
-    smooth = dsp.mix([score.opening(dsp.stf(360), 'sine') for i in range(3)])
+    rough = dsp.mix([score.opening(dsp.stf(600), 'random') for i in range(3)])
+    roughy = dsp.mix([score.opening(dsp.stf(600), 'line') for i in range(3)])
+    smooth = dsp.mix([score.opening(dsp.stf(600), 'sine') for i in range(3)])
     out += dsp.mix([rough, roughy, smooth], False, 0.8)
 
     out = dsp.write(out, 'render', True)
@@ -35,6 +35,7 @@ class Score:
         dsp.audio_params = dsp.default_params 
 
     def opening(self, length, type='sine', out=''):
+        print 'opening!', length, type
         salty = random.random() * 0.01
         low = dsp.mix([dsp.env(dsp.tone(length, (i+1)*25.0 + salty, type, random.random() * 0.2), 'sine') for i in range(8)])
         mid = dsp.mix([dsp.env(dsp.tone(length, (i+1)*37.51 + salty, type, random.random() * 0.2), 'sine') for i in range(8)])
