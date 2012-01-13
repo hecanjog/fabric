09437e2aa43500b9a116d8b51c1a459e4c9bc406
Erik Schoster <erik@heythere.org>
2012-01-13 17:10:27 -0600

    better blur harmony

--- reber.py
+++ reber.py
@@ -6,15 +6,18 @@ import fabric.fabric as dsp
 def main(out=''):
     dsp.timer('start') 
     dsp.snddir = 'sounds/reber/'
+    dsp.seed('Blur')
 
     orc = Orc()
 
-    out += orc.scrub([300 * i for i in range(1, 20)], dsp.stf(20), 'line')
-    out += orc.scrub([300 * i for i in range(1, 20)], dsp.stf(20), 'phasor')
-    out += orc.scrub([400 * i for i in range(1, 10)], dsp.stf(20), 'line')
-    out += orc.scrub([600 * i for i in range(1, 20)], dsp.stf(20), 'phasor')
+    layers = []
+    layers.append(orc.scrub([130 * 1.5 * i + dsp.rand(-1.0, 1.0) for i in range(1, 3)], dsp.stf(300), 'phasor', ('rand', dsp.mstf(1000))))
+    layers.append(orc.scrub([165.6 * 1.5 * i + dsp.rand(-1.0, 1.0) for i in range(1, 3)], dsp.stf(300), 'sine', ('rand', dsp.mstf(1500))))
+    layers.append(orc.scrub([220 * 1.5 * i + dsp.rand(-1.0, 1.0) for i in range(1, 3)], dsp.stf(300), 'line', ('rand', dsp.mstf(2500))))
 
-    out += dsp.mix([orc.expand(dsp.stf(3), 16, dsp.htf(9.5 * i), dsp.htf(300.5 * i)) for i in range(1, 10)], False)
+    layers = [dsp.env(layer, 'sine') for layer in layers]
+
+    out += dsp.mix(layers)
 
     out = dsp.write(out, 'blur', True)
     dsp.timer('stop')
@@ -25,25 +28,26 @@ class Orc:
     def __init__(self):
         self.blur = dsp.read('blur3.wav')
 
-    def expand(self, length, grid, longest, shortest, out=''):
-        out += ''.join([dsp.env(dsp.cut(self.blur.data, i * grid, v), 'sine', True) for i,v in enumerate(dsp.wavetable('random', length / grid, longest, shortest))])
-
-        return out
-
-    def scrub(self, pitches, length, wtype, out=''):
+    def scrub(self, pitches, length, wtype, sel=(0.0, 44100), out=''):
         layers = []
 
         for pitch in pitches:
             numcycles = length / dsp.htf(pitch)
 
+            if sel[0] == 'rand':
+                fstart = dsp.rand() * dsp.flen(self.blur.data) - dsp.htf(pitch)
+            else:
+                fstart = sel[0] * dsp.flen(self.blur.data) - dsp.htf(pitch)
+
+            fend = fstart + sel[1]
+
             if length % dsp.htf(pitch) > 0:
                 numcycles += 1
 
-            wtable = dsp.wavetable(wtype, numcycles, dsp.flen(self.blur.data) - dsp.htf(pitch), 0.0) 
-            #layers.append(''.join([dsp.env(dsp.cut(self.blur.data, int(i), dsp.htf(pitch)), 'sine') for i in wtable]))
-            layers.append(''.join([dsp.cut(self.blur.data, int(i), dsp.htf(pitch)) for i in wtable]))
+            wtable = dsp.wavetable(wtype, numcycles, fend, fstart)
+            layers.append(''.join([dsp.env(dsp.cut(self.blur.data, int(i), dsp.htf(pitch)), 'sine') for i in wtable]))
 
-        out += dsp.mix(layers)
+        out += dsp.mix(layers, True, 0.7)
 
         return out 
       
