fedfda487353149585d6927a88d33b6294aa8c50
Erik Schoster <erik@heythere.org>
2012-01-15 16:12:05 -0600

    burst sun

--- duet.py
+++ duet.py
@@ -13,14 +13,22 @@ def main(out=''):
     tonic = 250.0
 
     layers = []
-    layers.append(orc.scrub([tonic * i + dsp.rand(-1.0, 1.0) for i in range(1, 3)], dsp.stf(260), 'sine', (0.0, dsp.mstf(2500))))
-    layers.append(orc.scrub([tonic * 1.25 * i + dsp.rand(-1.0, 1.0) for i in range(1, 3)], dsp.stf(260), 'phasor', (0.5, dsp.mstf(2500))))
-    layers.append(orc.scrub([tonic * 1.5 * i + dsp.rand(-1.0, 1.0) for i in range(1, 3)], dsp.stf(260), 'sine', (0.1, dsp.mstf(2500))))
+    layers.append(orc.scrub([tonic + dsp.rand(-1.0, 1.0)], dsp.stf(245), 'sine', (0.1, dsp.mstf(2000))))
+    layers.append(orc.scrub([tonic * 2 + dsp.rand(-1.0, 1.0) ], dsp.stf(230), 'sine', (0.11, dsp.mstf(2000))))
+    layers.append(orc.scrub([tonic * 3 + dsp.rand(-1.0, 1.0) ], dsp.stf(215), 'sine', (0.12, dsp.mstf(2000))))
+    
+    layers.append(orc.scrub([tonic * 1.5 * i + dsp.rand(-1.0, 1.0) for i in range(1, 3)], dsp.stf(200), 'sine', (0.3, dsp.mstf(1500))))
 
-    layers.append(orc.scrub([tonic * 2.25 * i + dsp.rand(-1.0, 1.0) for i in range(1, 2)], dsp.stf(160), 'sine', (0.3, dsp.mstf(1500))))
+    layers.append(orc.scrub([tonic * 1.25 * i + dsp.rand(-1.0, 1.0) for i in range(1, 3)], dsp.stf(130), 'phasor', (0.2, dsp.mstf(1500))))
+    layers.append(orc.scrub([tonic * 2.25 * i + dsp.rand(-1.0, 1.0) for i in range(1, 2)], dsp.stf(140), 'sine', (0.4, dsp.mstf(2000))))
+    layers.append(orc.scrub([tonic * 2.25 * i + dsp.rand(-1.0, 1.0) for i in range(1, 2)], dsp.stf(140), 'sine', (0.5, dsp.mstf(1000))))
 
     layers = [dsp.env(layer, 'sine') for layer in layers]
-    out += dsp.mix(layers, False)
+
+    asun = orc.scrub([tonic * 4], dsp.stf(3), 'sine', (0.32, dsp.mstf(500)))
+    bsun = orc.scrub([tonic * 3], dsp.stf(3), 'sine', (0.3, dsp.mstf(500)))
+
+    out += dsp.mix([dsp.mix(layers, False), dsp.env(orc.burstsun(bsun, 550), 'phasor'), dsp.env(orc.burstsun(asun, 600), 'phasor')])
 
     out = dsp.write(out, 'duet', True)
     dsp.timer('stop')
@@ -29,7 +37,15 @@ class Orc:
     """ Do things, play things """
 
     def __init__(self):
-        self.blur = dsp.read('horn.wav')
+        self.horn = dsp.read('horn.wav')
+
+    def burstsun(self, snd, numtimes, out=''):
+        horns = [dsp.cut(snd, i * dsp.mstf(4), 2**i) for i in range(numtimes)]
+        horns = [dsp.panenv(h, 'random', 'sine', dsp.rand() * 0.5, dsp.rand() * 0.5 + 0.5) for h in horns]
+
+        out += ''.join(horns)
+
+        return out
 
     def scrub(self, pitches, length, wtype, sel=(0.0, 44100), out=''):
         layers = []
@@ -38,9 +54,9 @@ class Orc:
             numcycles = length / dsp.htf(pitch)
 
             if sel[0] == 'rand':
-                fstart = dsp.rand() * dsp.flen(self.blur.data) - dsp.htf(pitch)
+                fstart = dsp.rand() * dsp.flen(self.horn.data) - dsp.htf(pitch)
             else:
-                fstart = sel[0] * dsp.flen(self.blur.data) - dsp.htf(pitch)
+                fstart = sel[0] * dsp.flen(self.horn.data) - dsp.htf(pitch)
 
             fend = fstart + sel[1]
 
@@ -48,7 +64,7 @@ class Orc:
                 numcycles += 1
 
             wtable = dsp.wavetable(wtype, numcycles, fend, fstart)
-            layers.append(''.join([dsp.env(dsp.cut(self.blur.data, int(i), dsp.htf(pitch)), 'sine') for i in wtable]))
+            layers.append(''.join([dsp.env(dsp.cut(self.horn.data, int(i), dsp.htf(pitch)), 'sine') for i in wtable]))
 
         out += dsp.mix(layers, True, 2.0)
 
