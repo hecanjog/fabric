4ffc3fe7cb38fb3283739ed93552cf50ed2eebd6
Erik Schoster <erik@heythere.org>
2012-01-20 11:11:50 -0600

    replacing random elements with generative seed

--- pocket.py
+++ pocket.py
@@ -2,10 +2,6 @@
 # -*- coding: UTF-8 -*-
 
 import fabric.fabric as dsp
-import random
-import time
-import audioop 
-import math
 
 def main(out=''):
     """ sending dreams to she downstream """
@@ -20,7 +16,7 @@ def main(out=''):
 
     out += orc.bells_seqA()
 
-    out = dsp.write(out, 'render', True)
+    out = dsp.write(out, 'pocket', True)
     dsp.timer('stop')
 
 
@@ -51,26 +47,24 @@ class Orc:
         tone = dsp.tone(grain_size, freqs[0], 'sine2pi', 0.05)
         tone2 = dsp.tone(grain_size, freqs[1], 'saw', 0.05)
         tone = dsp.mix([tone, tone2])
-        out += ''.join([dsp.pulsar(tone, (1.0, 1.05, 'random'), (1.0, 1.0, 'line'), random.random()) for i in range(length / dsp.flen(tone))])
+        out += ''.join([dsp.pulsar(tone, (1.0, 1.05, 'random'), (1.0, 1.0, 'line'), dsp.rand()) for i in range(length / dsp.flen(tone))])
         return out
 
     def opening(self, length, amp=0.5, env_type='sine2pi', out=''):
         print 'opening!', length, env_type
 
-        salty = random.random() * 0.01
-        env_type = 'saw'
-        low = dsp.mix([dsp.env(dsp.tone(length, (i+1)*25.0 + salty, env_type, amp), 'sine') for i in range(8)])
-        mid = dsp.mix([dsp.env(dsp.tone(length, (i+1)*75.1 + salty, env_type, amp), 'sine') for i in range(24)])
-        env_type = 'random'
-        high = dsp.mix([dsp.env(dsp.tone(length, (i+1)*50.05 + salty, env_type, amp), 'sine') for i in range(24)])
-        higher = dsp.mix([dsp.env(dsp.tone(length, (i+1)*100.05 + salty, env_type, amp), 'sine') for i in range(12)])
+        low = dsp.mix([dsp.env(dsp.tone(length, (i+1)*25.0 + dsp.rand(0.0, 0.01), 'saw', amp), 'sine') for i in range(8)])
+        mid = dsp.mix([dsp.env(dsp.tone(length, (i+1)*75.1 + dsp.rand(0.0, 0.01), 'saw', amp), 'sine') for i in range(24)])
+
+        high = dsp.mix([dsp.env(dsp.tone(length, (i+1)*50.05 + dsp.rand(0.0, 0.01), 'random', amp), 'sine') for i in range(24)])
+        higher = dsp.mix([dsp.env(dsp.tone(length, (i+1)*100.05 + dsp.rand(0.0, 0.01), 'random', amp), 'sine') for i in range(12)])
 
         out += dsp.amp(dsp.mix([low,mid, dsp.env(dsp.mix([high, higher]), 'line')]), 0.7)
         
         return out
 
     def swells_opening(self, length, pitch, env='sine2pi', amp=0.15, fmod=1.05, out=''):
-        out += dsp.mix([dsp.pulsar(dsp.tone(length, pitch, env, amp), (1.0, fmod, 'random'), (0.6, 1.0, 'vary'), random.random()) for i in range(3)])
+        out += dsp.mix([dsp.pulsar(dsp.tone(length, pitch, env, amp), (1.0, fmod, 'random'), (0.6, 1.0, 'vary'), dsp.rand()) for i in range(3)])
         return out
 
     def bells_opening(self, out=''):
@@ -127,7 +121,7 @@ class Orc:
 
     def bell(self, freq, length, amp):
         bell = dsp.tone(length, freq, 'sine2pi', amp)
-        bell = dsp.mix([dsp.pulsar(bell, (1.0, 1.008, 'random'), (0.0, 1.0, 'phasor'), random.random()) for i in range(10)], True, 2.0)
+        bell = dsp.mix([dsp.pulsar(bell, (1.0, 1.008, 'random'), (0.0, 1.0, 'phasor'), dsp.rand()) for i in range(10)], True, 2.0)
 
         return bell
 
