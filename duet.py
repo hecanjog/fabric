4aac90c21480722a0b47743761f7d63df7e695e3
Erik Schoster <erik@heythere.org>
2012-01-30 14:01:17 -0600

    revisions to duet

--- duet.py
+++ duet.py
@@ -10,46 +10,52 @@ def main(out=''):
 
     orc = Orc()
 
-    tonic = 250.0
-    llen = dsp.stf(1200)
+    tonic = 110.0
+    llen = dsp.stf(1010)
     layers = []
 
     # Opening high tones
-    hi = orc.scrub([tonic * 12, tonic * 3, tonic * 6], int(llen * 0.25), 'random', (0.0, dsp.mstf(100)))
+    hi = orc.scrub([tonic * 2, tonic * 3, tonic * 6], int(llen * 0.25), 'random', (0.0, 0.5))
     hi = dsp.split(hi, int(llen * 0.0125))
-    hi = [dsp.env(h, 'sine') for h in hi]
-    layers.append(dsp.pad(dsp.mix([''.join(hi), orc.scrub([tonic * 6, tonic * 3], int(llen * 0.25), 'sine', (0.0, dsp.mstf(2000)), 'random')]), 0, int(llen * 0.74)))
+    hi = ''.join(dsp.randshuffle(hi))
+    hi = dsp.mix([orc.scrub([tonic * 3, tonic * 2], int(llen * 0.25), 'sine', (0.0, 0.4), 'random'), hi])
+    hi = dsp.env(hi, 'sine')
+    hi = dsp.pad(hi, 0, int(llen * 0.74))
+    layers.append(hi)
 
     # Opening tones P1, P8, P12
-    layers.append(orc.scrub([tonic + dsp.rand(-1.0, 1.0)], int(llen * 1.0), 'sine', (0.3, dsp.mstf(2000))))
-    layers.append(orc.scrub([tonic * 2 + dsp.rand(-1.0, 1.0) ], int(llen * 0.95), 'sine', (0.33, dsp.mstf(4000))))
-    layers.append(orc.scrub([tonic * 3 + dsp.rand(-1.0, 1.0) ], int(llen * 0.97), 'sine', (0.32, dsp.mstf(2000))))
-
-    layers = [dsp.env(layer, 'sine') for layer in layers]
-    layers[0] = dsp.mix(layers, False, 4.0) # Preventing out of memory errors
+    subgroup = []
+    subgroup.append(orc.scrub([tonic + dsp.rand(-1.0, 1.0)], int(llen * 1.0), 'sine', (0.0, 0.3)))
+    subgroup.append(orc.scrub([tonic * 2 + dsp.rand(-1.0, 1.0) ], int(llen * 0.95), 'sine', (0.4, 0.4)))
+    subgroup.append(orc.scrub([tonic * 3 + dsp.rand(-1.0, 1.0) ], int(llen * 0.97), 'sine', (0.02, 0.2)))
+    layers.append(dsp.env(dsp.mix(subgroup, False, 4.0), 'sine'))
    
     # Middle tones, rougher P5, P8
-    layers.append(orc.scrub([tonic * 1.5 * i + dsp.rand(-1.0, 1.0) for i in range(1, 3)], int(llen * 0.7), 'sine', (0.3, dsp.mstf(5500))))
-    layers.append(orc.scrub([tonic * 2 * i + dsp.rand(-1.0, 1.0) for i in range(1, 2)], int(llen * 0.7), 'sine', (0.35, dsp.mstf(6500)), 'gauss'))
-    layers.append(orc.scrub([tonic * 2 * i + dsp.rand(-1.0, 1.0) for i in range(1, 2)], int(llen * 0.75), 'sine', (0.4, dsp.mstf(4500)), 'phasor'))
-
-    layers[0] = dsp.mix(layers, False, 4.0)
+    subgroup = []
+    subgroup.append(orc.scrub([tonic * 1.5 * i + dsp.rand(-1.0, 1.0) for i in range(1, 3)], int(llen * 0.7), 'sine', (0.3, 0.75)))
+    subgroup.append(orc.scrub([tonic * 2 * i + dsp.rand(-1.0, 1.0) for i in range(1, 2)], int(llen * 0.7), 'sine', (0.35, 0.4), 'gauss'))
+    subgroup.append(orc.scrub([tonic * 2 * i + dsp.rand(-1.0, 1.0) for i in range(1, 2)], int(llen * 0.75), 'sine', (0.4, 0.5), 'phasor'))
+    layers.append(dsp.env(dsp.mix(subgroup, False, 4.0), 'sine'))
 
     # End tones, M3, M9
-    layers.append(orc.scrub([tonic * 1.25 * i + dsp.rand(-1.0, 1.0) for i in range(1, 3)], int(llen * 0.45), 'phasor', (0.2, dsp.mstf(4500))))
-    layers.append(orc.scrub([tonic * 2.25 * i + dsp.rand(-1.0, 1.0) for i in range(1, 2)], int(llen * 0.55), 'sine', (0.4, dsp.mstf(4000))))
-    layers.append(orc.scrub([tonic * 2.25 * i + dsp.rand(-1.0, 1.0) for i in range(1, 2)], int(llen * 0.5), 'sine', (0.5, dsp.mstf(5000)), 'random'))
-
-    layers[0] = dsp.mix(layers, False, 4.0)
+    subgroup = []
+    subgroup.append(orc.scrub([tonic * 1.25 * i + dsp.rand(-1.0, 1.0) for i in range(1, 3)], int(llen * 0.45), 'phasor', (0.2, 0.1), 'gauss'))
+    subgroup.append(orc.scrub([tonic * 2.25 * i + dsp.rand(-1.0, 1.0) for i in range(1, 2)], int(llen * 0.55), 'sine', (0.4, 0.3), 'random'))
+    subgroup.append(orc.scrub([tonic * 2.25 * i + dsp.rand(-1.0, 1.0) for i in range(1, 2)], int(llen * 0.5), 'sine', (0.5, 0.4), 'random'))
+    subgroup = [dsp.split(s, dsp.mstf(80 + (i*2))) for s in subgroup]
+    subgroup = [''.join(dsp.randshuffle(s)) for s in subgroup]
+    layers.append(dsp.env(dsp.mix(subgroup, False, 4.0), 'sine'))
 
     # Coda tones, low tonics
-    layers.append(orc.scrub([tonic * 0.25, tonic * 0.125], int(llen * 0.2), 'line', (0.35, dsp.mstf(2500))))
-    layers.append(orc.scrub([tonic * 0.25, tonic * 0.5], int(llen * 0.2), 'line', (0.37, dsp.mstf(1500)), 'random'))
+    subgroup = []
+    subgroup.append(orc.scrub([tonic * 0.25, tonic * 0.125], int(llen * 0.2), 'line', (0.35, 0.1)))
+    subgroup.append(orc.scrub([tonic * 0.25, tonic * 0.5], int(llen * 0.2), 'line', (0.37, 1.0), 'random'))
+    layers.append(dsp.env(dsp.mix(subgroup, False, 4.0), 'sine'))
 
-    thesun = orc.scrub([tonic * 50, tonic, tonic * 49, tonic * 48, tonic * 24 ], dsp.stf(10), 'sine', (0.32, dsp.mstf(3000)), 'random')
+    thesun = orc.scrub([tonic * 50, tonic, tonic * 49, tonic * 48, tonic * 24 ], dsp.stf(10), 'sine', (0.32, 0.4), 'random')
     suns = dsp.mix([orc.burstsun(dsp.amp(thesun, 0.9), int(llen * 1.0)) for i in range(6)])
 
-    out += dsp.env(dsp.mix([dsp.mix(layers, False, 4.0), suns], False), 'sine')
+    out += dsp.mix([dsp.mix(layers, False, 4.0), suns], False)
 
     out = dsp.write(out, 'duet', True)
     dsp.timer('stop')
@@ -58,7 +64,7 @@ class Orc:
     """ Do things, play things """
 
     def __init__(self):
-        self.horn = dsp.read('horn.wav')
+        self.horn = dsp.read('william/william.wav')
 
     def burstsun(self, snd, length, out=''):
         numtimes = int(length / dsp.mstf(1000))
@@ -70,7 +76,7 @@ class Orc:
 
         return out
 
-    def scrub(self, pitches, length, wtype, sel=(0.0, 44100), gtype='sine', out=''):
+    def scrub(self, pitches, length, wtype, sel=(0.0, 1.0), gtype='sine', out=''):
         layers = []
 
         for pitch in pitches:
@@ -81,7 +87,7 @@ class Orc:
             else:
                 fstart = sel[0] * dsp.flen(self.horn.data) - dsp.htf(pitch)
 
-            fend = fstart + sel[1]
+            fend = int(fstart + sel[1] * (dsp.flen(self.horn.data) - dsp.htf(pitch) - fstart))
 
             if length % dsp.htf(pitch) > 0:
                 numcycles += 1
