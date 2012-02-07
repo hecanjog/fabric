a550160b3a828576048f9aa3ce944cef522e4d86
Erik Schoster <erik@heythere.org>
2012-02-07 08:36:22 -0600

    morning haiku: money

--- haiku.py
+++ haiku.py
@@ -2,7 +2,12 @@ import fabric.fabric as dsp
 
 dsp.timer('start') 
 
-sweeps = [dsp.wavetable('gauss', 1000, 700 + (i * 30), 20) for i in range(10)]
+# This morning I am thinking about money.
+# Insane thoughts don't collectively produce insane realities, 
+# they produce insane behaviors, like money.
+
+points = [['random', dsp.rand(700 + (i * dsp.rand(10, 80)), 20)] for i in range(20)]
+sweeps = [dsp.breakpoint(points, 1000) for i in range(10)] 
 sweeps = [[dsp.cycle(f) for f in freqs] for freqs in sweeps]
 sweeps = [''.join(s) for s in sweeps]
 
