0e2583509132b38b6472c28a270b3802997fd2fb
Erik Schoster <erik@heythere.org>
2012-02-08 11:42:31 -0600

    late morning haiku: placements

--- haiku.py
+++ haiku.py
@@ -1,18 +1,21 @@
 import fabric.fabric as dsp
 
 dsp.timer('start') 
+dsp.seed('Placements')
+psize = 5777 
 
-# This morning I am thinking about money.
-# Insane thoughts don't collectively produce insane realities, 
-# they produce insane behaviors, like money.
+# A few hours in Milwaukee
 
-points = [['random', dsp.rand(700 + (i * dsp.rand(10, 80)), 20)] for i in range(20)]
-sweeps = [dsp.breakpoint(points, 1000) for i in range(10)] 
-sweeps = [[dsp.cycle(f) for f in freqs] for freqs in sweeps]
+etypes = ['line','sine','cos','impulse','sine2pi','cos2pi','saw','tri','flat']
+points = [[dsp.randchoose(etypes), dsp.rand(977 + (i * dsp.rand(11, 77)), 77)] for i in range(psize)]
+sweeps = [dsp.breakpoint(dsp.randshuffle(points), psize * 777) for i in range(7)] 
+sweeps = [[dsp.cycle(f, 'tri') for f in freqs] for freqs in sweeps]
 sweeps = [''.join(s) for s in sweeps]
+sweeps = [dsp.pan(s, dsp.rand(0.0, 1.0)) for s in sweeps]
 
-out = dsp.mix(sweeps, False, 0.5)
+out = dsp.mix(sweeps, False, 0.5) # Right-aligned
+out += dsp.mix(sweeps, True, 0.5) # Left-aligned
 
-print dsp.write(out, 'haiku', True)
+print dsp.write(out, 'haiku-12-02-08-placements', True)
 
 dsp.timer('stop')
