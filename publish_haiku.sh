02a660e7cd2fd9c4e49ed62c530a237227753607
Erik Schoster <erik@heythere.org>
2012-02-09 16:27:49 -0600

    fix to haiku publish script

--- publish_haiku.sh
+++ publish_haiku.sh
@@ -3,6 +3,7 @@ rm renders/haiku*
 python haiku.py
 
 for f in renders/haiku*; do
-    sox -S $f -C 320 $m/he.can.jog/haiku/${f/.wav/.mp3} norm
-    scp $m/he.can.jog/haiku/${f/.wav/.mp3} hecanjog@hecanjog.com:~/sounds.hecanjog.com/haiku/
+    fm=${f/renders\///};
+    sox -S $f -C 320 $m/he.can.jog/haiku/${fm/.wav/.mp3} norm
+    scp $m/he.can.jog/haiku/${fm/.wav/.mp3} hecanjog@hecanjog.com:~/sounds.hecanjog.com/haiku/
 done
