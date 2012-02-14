f43e2cc374dfec239f52a36984028c0696ad37e1
Erik Schoster <erik@heythere.org>
2012-02-14 14:03:03 -0600

    Part 1 of 2 part haiku: crossing

--- haiku.py
+++ haiku.py
@@ -1,26 +1,76 @@
 import fabric.fabric as dsp
+import audioop
 
 dsp.timer('start') 
-dsp.seed('together')
+dsp.seed('crossing')
 
-# Coming together
+# I've been meaning to add a feature to Fabric to let me easily split a 
+# sound by zero crossings. So! Here is a janky first test. Tomorrow I hope 
+# to do some bug fixes and optimize, because janky does not begin to describe...
+#
+# I think the problem is that my zero crossings rarely nail zero exactly on the way from 
+# negative to positive amplitudes. I should have guessed that!
+#
+# The function I wrote to test for crossings will have to be a little smarter
+# and look for values within a certiain threshold - or even better, transitions 
+# between positive and negative amplitudes.
+#
+# That will have to be for tomorrow, unless I find a little time to work on it tonight.
+#
+# In the meantime, I'm cheating and attenuating the input to create more obvious zeros.
 
-curves = [dsp.wavetable('line', 1000 + dsp.randint(0,500)) for i in range(100)]
-curves = [[int(dsp.mstf(10) + (i * dsp.mstf(190))) for i in c] for c in curves]
+tonic = 500.0
 
-tonic = 100
+etypes = ['line', 'sine', 'cos', 'phasor', 'flat', 'gauss']
+ratios = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0, 3.5, 4.0, 5.0, 6.0, 8.0]
+pitches = [tonic * dsp.randchoose(ratios) + i for i in range(20)]
+stack = [dsp.tone(dsp.stf(30), pitches[i]) for i in range(20)]
+stack = [dsp.panenv(s, dsp.randchoose(etypes)) for s in stack]
+stack = [dsp.env(s, dsp.randchoose(etypes)) for s in stack]
+stack = dsp.mix(stack)
+stack = dsp.amp(stack, 0.01)
 
-tones = []
-for i,c in enumerate(curves):
-    pitch = tonic * (i+1)
-    tone = dsp.tone(dsp.mstf(200), pitch)
-    c = [dsp.cut(tone, 0, bleep) for bleep in c]
-    c = [dsp.env(bleep, 'phasor') for bleep in c]
-    c = [dsp.pan(bleep, dsp.rand()) for bleep in c]
-    tones += [ ''.join(c) ]
+def tomono(s):
+    left = audioop.tomono(s, dsp.audio_params[1], 1, 0)
+    right = audioop.tomono(s, dsp.audio_params[1], 0, 1)
 
-out = dsp.mix(tones, False)
+    return [left, right] 
 
-print dsp.write(out, 'haiku-12-02-13-together', False)
+def stereomix(s):
+    s[0] = audioop.tostereo(s[0], dsp.audio_params[1], 1, 0)
+    s[1] = audioop.tostereo(s[1], dsp.audio_params[1], 0, 1)
+    return dsp.mix(s) 
+
+def getchunks(s):
+    # the brute force approach...
+    all = dsp.split(s, 1)
+    chunk, chunks = [], []
+    zero = dsp.byte_string(0) * dsp.audio_params[1]
+
+    for frame in all:
+        if chunk == []:
+            chunk += [ frame ]
+        elif frame != zero and chunk != []:
+            chunk += [ frame ]
+        elif frame == zero and chunk != []:
+            chunk += [ frame ]
+            chunks += [ ''.join(chunk) ]
+            chunk = []
+
+    print len(chunks)
+
+    return chunks
+
+def shufflecrossings(s):
+    chans = tomono(s)
+    chans = [getchunks(c) for c in chans]
+    chans = [dsp.randshuffle(c) for c in chans]
+    chans = [''.join(c) for c in chans]
+
+    return stereomix(chans)
+
+out = dsp.amp(shufflecrossings(stack), 20)
+
+print dsp.write(out, 'haiku-12-02-14-crossing', False)
 
 dsp.timer('stop')
