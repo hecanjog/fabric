e39e5f3456e6a7e20be559de9878195edf512f07
Erik Schoster <erik@heythere.org>
2012-02-25 08:34:50 -0600

    lifting

--- haiku.py
+++ haiku.py
@@ -1,29 +1,24 @@
 import dsp
-import time
-import sys
 
-# A very barebones haiku, but baby steps toward just-in-realtime 
-# features for a performance at the end of March.
-#
-# Source sound by Meg Karls:
-# http://sounds.hecanjog.com/violin-d.wav
+dsp.timer('start')
+dsp.seed('lifting')
 
-dsp.seed('sleep')
+# Back to basics.
 
-print sys.argv
+etypes = ['line', 'sine', 'gauss', 'phasor', 'cos']
 
-violin = dsp.read('sounds/violin-d.wav')
-vlen = dsp.flen(violin.data)
-slen = dsp.mstf(280)
-violin = [dsp.cut(violin.data, dsp.randint(0, vlen - slen), slen) for i in range(100)]
-scale = [0.5, 0.75, 0.938, 1.0, 2.0]
+def stream(partial):
+    around = dsp.tone(dsp.stf(3), 222 * partial)
+    around = dsp.split(around, 0, 2)
 
-out = ''
-for i,v in enumerate(violin): 
-    if i < len(violin) / 2:
-        v = dsp.transpose(v, dsp.randchoose(scale))
-    
-    v = dsp.env(dsp.pan(v, dsp.rand()), 'random')
-    time.sleep(dsp.rand())
-    
-    out += dsp.play(v)
+    dsp.audio_params[0] = 1
+    within = dsp.cycle(777)
+    around = [''.join([a + within * dsp.randint(2, 64) for a in aa]) for aa in around]
+    dsp.audio_params[0] = 2 
+
+    return dsp.env(dsp.mixstereo(around), dsp.randchoose(etypes))
+
+out = dsp.mix([stream(i) for i in range(1,5)])
+
+print dsp.write(out, 'haiku-12-02-24-lifting', False)
+dsp.timer('stop')
