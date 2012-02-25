a67f3151fb3ae3fdeb098cdb4bc6fb6f81e77836
Erik Schoster <erik@heythere.org>
2012-02-25 09:00:20 -0600

    gathering

--- haiku.py
+++ haiku.py
@@ -1,24 +1,24 @@
 import dsp
 
 dsp.timer('start')
-dsp.seed('lifting')
+dsp.seed('gathering')
 
-# Back to basics.
+# Gathering toward 
 
 etypes = ['line', 'sine', 'gauss', 'phasor', 'cos']
 
 def stream(partial):
-    around = dsp.tone(dsp.stf(3), 222 * partial)
+    around = dsp.pulsar(dsp.tone(dsp.stf(20), 444 * partial))
     around = dsp.split(around, 0, 2)
 
-    dsp.audio_params[0] = 1
-    within = dsp.cycle(777)
-    around = [''.join([a + within * dsp.randint(2, 64) for a in aa]) for aa in around]
-    dsp.audio_params[0] = 2 
+    within = dsp.tone(dsp.stf(20), 444 * (1.0 / partial))
+    within = dsp.split(within, 0, 2)
 
-    return dsp.env(dsp.mixstereo(around), dsp.randchoose(etypes))
+    together = [''.join(dsp.interleave(around[i], within[i])) for i in range(2)]
 
-out = dsp.mix([stream(i) for i in range(1,5)])
+    return dsp.env(dsp.mixstereo(together), dsp.randchoose(etypes))
 
-print dsp.write(out, 'haiku-12-02-24-lifting', False)
+out = dsp.mix([stream(i) for i in range(1,7)])
+
+print dsp.write(out, 'haiku-12-02-25-gathering', False)
 dsp.timer('stop')
