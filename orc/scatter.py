df3c2537eaadc0e6385320d6ac22d72801208a7f
Erik Schoster <erik@heythere.org>
2012-02-24 21:47:45 -0600

    tweaks to scatter

--- orc/scatter.py
+++ orc/scatter.py
@@ -2,16 +2,25 @@ import dsp
 import time
 import sys
 
-if len(sys.argv) > 1:
-    notes = int(sys.argv[1])
-else:
-    notes = 1
+fixed = True
+notes = 1
+
+args = [arg for arg in sys.argv if arg != '']
+
+if len(args) > 1:
+    notes = int(args[1])
+
+if len(args) > 2:
+    fixed = False
+
+if fixed:
+    dsp.seed('fixed')
 
 violin = dsp.read('sounds/violin-d.wav')
 vlen = dsp.flen(violin.data)
-slen = dsp.mstf(280)
+slen = dsp.mstf(dsp.randint(1, 2000))
 violin = [dsp.cut(violin.data, dsp.randint(0, vlen - slen), slen) for i in range(notes)]
-scale = [0.5, 0.75, 0.938, 1.0, 2.0]
+scale = [0.5, 1.0, 2.0, 3.0]
 
 out = ''
 for i,v in enumerate(violin): 
@@ -19,6 +28,6 @@ for i,v in enumerate(violin):
         v = dsp.transpose(v, dsp.randchoose(scale))
     
     v = dsp.env(dsp.pan(v, dsp.rand()), 'random')
-    time.sleep(dsp.rand())
+    time.sleep(dsp.rand(0.0, 0.2))
     
     out += dsp.play(v)
