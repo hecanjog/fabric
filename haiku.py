207ca33c3cf1d6b0b99e71ae05199abc0a5912c7
Erik Schoster <erik@heythere.org>
2012-02-29 09:01:04 -0600

    destroy empire

--- haiku.py
+++ haiku.py
@@ -1,15 +1,18 @@
 import dsp
 
 dsp.timer('start')
-dsp.seed('empire builder')
+dsp.seed('destroy empire')
 
-# Pulse trains in Minnesota
+# Ray guns! Spaceship engines! Hints of the THX sound!
+#
+# Not sure how that happened.
 
 def pulsetrain(numpulses):
     impulse = dsp.byte_string(32767) * 2 
-    maxlength = dsp.randint(1000, 2000)
+    maxlength = dsp.randint(10, 300)
 
-    lengths = dsp.wavetable('cos', numpulses)
+    points = [0.0] + [['line', dsp.rand()] for i in range(30)]
+    lengths = dsp.breakpoint(points, numpulses)
     lengths = [int(i * maxlength) for i in lengths]
     pulses = [dsp.pan(impulse, dsp.rand()) for i in range(numpulses)]
     pulses = [dsp.pad(pulse, 0, lengths[i]) for i, pulse in enumerate(pulses)]
@@ -18,7 +21,7 @@ def pulsetrain(numpulses):
 
     return pulses
 
-out = dsp.mix([pulsetrain(10000) for i in range(10)], False)
+out = dsp.mix([pulsetrain(20000) for i in range(100)], False, 10.0)
 
-print dsp.write(out, 'haiku-12-02-28-empire-builder', False)
+print dsp.write(out, 'haiku-12-02-29-destroy-empire', False)
 dsp.timer('stop')
