35b93d747aa2cf5a07e0763ca666f725e557a237
Erik Schoster <erik@heythere.org>
2012-03-03 12:37:14 -0600

    beed

--- haiku.py
+++ haiku.py
@@ -1,23 +1,24 @@
 import dsp
 
 dsp.timer('start')
-dsp.seed('beec')
+dsp.seed('beed')
 
-# Busy bees
-# I revised this over 4+ hours, and ended up with
-# almost nothing to show for it. So it goes!
+# Neat!
 
 def bee(tonic, out=''):
-    t = dsp.stf(120) / dsp.htf(tonic);
+    t = dsp.stf(20) / dsp.htf(tonic);
+    s = dsp.wavetable('line', t)
+    wtable = dsp.wavetable('sine2pi', dsp.htf(tonic))
+    div = dsp.randint(100, 1000)
 
     for i in range(t):
-        wtable = dsp.wavetable('line', dsp.htf(tonic))
-        out += dsp.pan(''.join([dsp.pack(0.4 * f * 2.0 - 1.0) * 2 for f in wtable]), dsp.rand())
+        fp = (i % div) * 0.1 + 0.0001
+        out += ''.join([dsp.pack(0.4 * f + (s[i] / (f + fp))) * 2 for f in wtable])
 
     return out
 
 out = dsp.mix([bee(100 * i) for i in range(1, 10)], False)
 
-print dsp.write(out, 'haiku-12-03-02-beec', False)
+print dsp.write(out, 'haiku-12-03-03-beed', False)
 dsp.timer('stop')
 
