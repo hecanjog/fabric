1cabff908ecf0e34f71707a8c3340b31f8ab8704
Erik Schoster <erik@heythere.org>
2012-03-03 12:01:26 -0600

    busy bees

--- haiku.py
+++ haiku.py
@@ -1,14 +1,23 @@
 import dsp
 
 dsp.timer('start')
+dsp.seed('beec')
 
-out = ''
-thirty = 44100 * 2 * 2 * 30;
+# Busy bees
+# I revised this over 4+ hours, and ended up with
+# almost nothing to show for it. So it goes!
 
-for i in range(thirty):
-    d = i % (thirty / 100) + 1
-    s = i % d
-    out += chr(s % 256)
+def bee(tonic, out=''):
+    t = dsp.stf(120) / dsp.htf(tonic);
 
-print dsp.write(out, 'haiku-12-03-01-thirtyc', False)
+    for i in range(t):
+        wtable = dsp.wavetable('line', dsp.htf(tonic))
+        out += dsp.pan(''.join([dsp.pack(0.4 * f * 2.0 - 1.0) * 2 for f in wtable]), dsp.rand())
+
+    return out
+
+out = dsp.mix([bee(100 * i) for i in range(1, 10)], False)
+
+print dsp.write(out, 'haiku-12-03-02-beec', False)
 dsp.timer('stop')
+
