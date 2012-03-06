d043931785e86b30915cd8a054ec5a46b3408f7d
Erik Schoster <erik@heythere.org>
2012-03-05 18:12:45 -0600

    avian

--- haiku.py
+++ haiku.py
@@ -1,23 +1,36 @@
 import dsp
 
 dsp.timer('start')
-dsp.seed('avian')
+dsp.seed('Avian')
 
 birds = dsp.read('sounds/nsong.wav')
 birds = dsp.split(birds.data, dsp.stf(10)) * 2 
-birds = birds[:15]
+birds = birds[:12]
 
 guitar = dsp.read('sounds/cguitar.wav')
 
-def gcut(guitar, i):
-    g = dsp.cut(guitar, dsp.stf(i * 0.1 + 0.6), dsp.stf(i * 0.1 + 0.1))
-    g = dsp.fill(g, dsp.stf(250))
-    g = dsp.pan(g, dsp.rand())
-    g = dsp.env(g, 'vary')
+def gcut(guitar, i, j=0):
+    etypes = ['sine', 'line', 'phasor', 'tri']
+    etypes += ['sine' for e in range(10)]
+
+    t = dsp.stf(dsp.rand(20, 230))
+    if t < dsp.stf(160):
+        j = 20 * i
+        if j % 3 == 0:
+            guitar = dsp.amp(guitar, dsp.rand(1.0, 6.0))
+
+    g = dsp.cut(guitar, dsp.stf((i + j) * 0.005 + 0.6), dsp.stf(i * 0.03 + 0.01))
+    g = dsp.fill(g, t)
+    g = dsp.split(g, dsp.flen(g) / dsp.randint(2, 32))
+    g = [dsp.pan(s, dsp.rand()) for s in g]
+    g = [dsp.env(s, dsp.randchoose(etypes)) for s in g]
+    g = [dsp.amp(s, dsp.rand(0.05, 3.0)) for s in g]
+    g = [dsp.pad(s, dsp.randint(0, t / 3), dsp.randint(0, t / 3)) for s in g]
+    g = dsp.fill(''.join(g), t)
 
     return g
 
-guitar = dsp.mix([gcut(guitar.data, i) for i in range(100)], True, 30.0)
+guitar = dsp.mix([gcut(guitar.data, i) for i in range(150)], False, 20.0)
 guitar = dsp.split(guitar, dsp.flen(guitar) / 3)
 guitar = dsp.env(guitar[0], 'line') + guitar[1] + dsp.env(guitar[2], 'phasor')
 
